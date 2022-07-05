74f50cd4223adf321bd36e26dc453107419931f4
retroninja <59034596+RetroNynjah@users.noreply.github.com>
2022-07-06 00:12:26 +0200

    Replaced 1541-II_RomSwitcher_Sketch.ino
    Wrong file uploaded

--- rev2/firmware/1541-II_RomSwitcher_Sketch.ino
+++ rev2/firmware/1541-II_RomSwitcher_Sketch.ino
@@ -6,6 +6,10 @@
 // The below reversed searchString in hex ascii is MORNR@ which is specified like this on the C64: 1@RNROM, 2@RNROM and so on.
 byte searchString[] = {0x4D,0x4F,0x52,0x4E,0x52,0x40};
 
+int resetPin = 11;
+int clockPin = 13;
+int ledPin = A0;
+
 int commandLength = sizeof(searchString);
 int bytesCorrect = 0;
 volatile bool state;
@@ -36,47 +40,71 @@ void cleareeprom(){
 
 void resetdrive(){
   
-  pinMode(11, OUTPUT); // reset pin
-  digitalWrite(11, LOW);
+  pinMode(resetPin, OUTPUT); // reset pin
+  digitalWrite(resetPin, LOW);
   delay(50);
-  digitalWrite(11, HIGH);
+  digitalWrite(resetPin, HIGH);
   delay(50);
-  digitalWrite(11, LOW);
-  pinMode(11, INPUT);
+  digitalWrite(resetPin, LOW);
+  pinMode(resetPin, INPUT);
 
 }
 
 
 void switchrom(int romnumber){
 
-  for (int x = 0; x <= romnumber; x++){
-    digitalWrite (A0, HIGH);
-    delay(30);
-    digitalWrite (A0, LOW);
-    delay(250);
-  }
-  delay (200);
-
-  // switch eprompin A14 (D9)
-  if (romnumber & B01){
-    digitalWrite(9 , HIGH);
+  // switch eprompin A14 (D8)
+  if (romnumber & B00001){
+    digitalWrite(8, HIGH);
   }  
   else {
-    digitalWrite(9 , LOW);
+    digitalWrite(8, LOW);
   }
 
-  // switch eprompin A15 (D10)
-  if (romnumber & B10){
-    digitalWrite(10 , HIGH);
+  // switch eprompin A15 (D9)
+  if (romnumber & B00010){
+    digitalWrite(9, HIGH);
   }  
   else {
-    digitalWrite(10 , LOW);
+    digitalWrite(9, LOW);
+  }
+
+  // switch eprompin A16 (D10)
+  if (romnumber & B00100){
+    digitalWrite(10, HIGH);
+  }  
+  else {
+    digitalWrite(10, LOW);
+  }
+
+  // switch eprompin A17 (A1)
+  if (romnumber & B01000){
+    digitalWrite(A1, HIGH);
+  }  
+  else {
+    digitalWrite(A1, LOW);
+  }
+
+  // switch eprompin A18 (A2)
+  if (romnumber & B10000){
+    digitalWrite(A2, HIGH);
+  }  
+  else {
+    digitalWrite(A2, LOW);
   }
 
   int savedROM = EEPROM.read(0);
   if (savedROM != romnumber){
     EEPROM.write(0, romnumber);
   }
+
+  for (int x = 0; x <= romnumber; x++){
+    digitalWrite (ledPin, HIGH);
+    delay(30);
+    digitalWrite (ledPin, LOW);
+    delay(250);
+  }
+  delay (200);
 
   resetdrive();
 
@@ -91,33 +119,33 @@ void blinknumber(int num) {
   int singular = num % 10;
   
   for (int x = 1; x <= 10; x++){
-    digitalWrite(A0, HIGH);
+    digitalWrite(ledPin, HIGH);
     delay(50);
-    digitalWrite(A0, LOW);
+    digitalWrite(ledPin, LOW);
     delay(50);
   }
   delay(2000);
 
   for (int x = 1; x <= hundreds; x++){
-    digitalWrite(A0, HIGH);
+    digitalWrite(ledPin, HIGH);
     delay(50);
-    digitalWrite(A0, LOW);
+    digitalWrite(ledPin, LOW);
     delay(350);
   }
   delay(2000);
 
   for (int x = 1; x <= tens; x++){
-    digitalWrite(A0, HIGH);
+    digitalWrite(ledPin, HIGH);
     delay(50);
-    digitalWrite(A0, LOW);
+    digitalWrite(ledPin, LOW);
     delay(350);
   }
   delay(2000);
 
   for (int x = 1; x <= singular; x++){
-    digitalWrite(A0, HIGH);
+    digitalWrite(ledPin, HIGH);
     delay(50);
-    digitalWrite(A0, LOW);
+    digitalWrite(ledPin, LOW);
     delay(350);
   }
 }
@@ -129,11 +157,14 @@ void setup() {
   // set data pins as inputs
   DDRD = B00000000;
   
-  pinMode(13, INPUT); // clock pin
-  pinMode(9, OUTPUT); // eprom A14
-  pinMode(10, OUTPUT); // eprom A15
-  pinMode(11, INPUT); // reset pin
-  pinMode (A0, OUTPUT); // LED
+  pinMode(8, OUTPUT); // eprom A14
+  pinMode(9, OUTPUT); // eprom A15
+  pinMode(10, OUTPUT); // eprom A16
+  pinMode(A1, OUTPUT); // eprom A17
+  pinMode(A2, OUTPUT); // eprom A18
+  pinMode(resetPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  pinMode(clockPin, INPUT);
   
   // retrieve last used ROM from ATmega EEPROM and switch ROM using A14/A15
   int lastROM = EEPROM.read(0);
@@ -144,14 +175,14 @@ void setup() {
   switchrom(lastROM);
   
   // enable pin change interrupt on pin D13(PB5) connected to R/W on 6502
-  pciSetup(13); 
+  pciSetup(clockPin);
 }
 
 
 void loop() {
   if (state == HIGH){
-    state=LOW;
     byte byteCurr = PIND;
+    state=LOW;
       
     if (bytesCorrect == commandLength){
       // we have our search string. This byte must be the ROM number
